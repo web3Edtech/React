b6527d1e64ed127ae6b71fbd6d585f1cc67900f5
web3Edtech <webonwardsedtech@gmail.com>
2024-10-10 18:33:14 +0530

    Add files via upload

---
+++ Task1_Mastering React Basics and CLI Tools/src/App.js
@@ -0,0 +1,13 @@
+// src/App.js
+import React from 'react';
+import WelcomeBanner from './components/WelcomeBanner';
+
+function App() {
+    return (
+        <div className="App">
+            <WelcomeBanner />
+        </div>
+    );
+}
+
+export default App;
